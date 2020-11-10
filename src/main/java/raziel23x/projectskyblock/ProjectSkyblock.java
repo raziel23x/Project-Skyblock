a9506a386172f47f5f69a4066b0f99e1a9be3c48
Gerald Baggett <raziel23x@gmail.com>
2020-11-10 11:49:58 -0500

    Forge 34.1.42 MDK

---
+++ src/main/java/raziel23x/projectskyblock/ProjectSkyblock.java
@@ -0,0 +1,12 @@
+package raziel23x.projectskyblock;
+
+import net.minecraftforge.fml.common.Mod;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@Mod("projectskyblock")
+public class ProjectSkyblock {
+    public static final String MOD_ID = "projectskyblock";
+    public static final Logger LOGGER = LogManager.getLogger("Project Skyblock");
+
+}
