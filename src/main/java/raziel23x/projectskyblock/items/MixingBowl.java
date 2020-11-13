2681f157627541850a55671b86e9a72a3c8b8a2f
ValkyrieofNight <thevalkyrieofnight@gmail.com>
2020-11-12 17:16:46 -0800

    Update MixingBowl.java

--- src/main/java/raziel23x/projectskyblock/items/MixingBowl.java
+++ src/main/java/raziel23x/projectskyblock/items/MixingBowl.java
@@ -7,15 +7,16 @@ import raziel23x.projectskyblock.utils.RegistryHandler;
 
 public class MixingBowl extends ItemBase {
     public MixingBowl () {
+        super(new Item.Properties().group(ProjectSkyblock.TAB));
+    }
 
-        @Override
-        public boolean hasContainerItem (ItemStack stack){
-            return true;
-        }
+    @Override
+    public boolean hasContainerItem (ItemStack stack){
+        return true;
+    }
 
-        @Override
-        public ItemStack getContainerItem (ItemStack stack){
-            return stack.copy();
-        }
+    @Override
+    public ItemStack getContainerItem (ItemStack stack){
+        return stack.copy();
     }
 }
