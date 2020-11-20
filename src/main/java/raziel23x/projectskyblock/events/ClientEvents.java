ecbcd58496b536e478a4576557593d50c2c151df
Gerald Baggett <raziel23x@gmail.com>
2020-11-20 14:01:48 -0500

    Fixed water gen to allow the biome to effect the color of the minture water block in the model as well as held

--- src/main/java/raziel23x/projectskyblock/events/ClientEvents.java
+++ src/main/java/raziel23x/projectskyblock/events/ClientEvents.java
@@ -1,42 +1,44 @@
 package raziel23x.projectskyblock.events;
 
-import net.minecraft.client.Minecraft;
-import net.minecraft.client.renderer.RenderType;
-import net.minecraft.client.renderer.RenderTypeLookup;
+import net.minecraft.block.BlockState;
 import net.minecraft.client.renderer.color.BlockColors;
+import net.minecraft.client.renderer.color.ItemColors;
+import net.minecraft.item.BlockItem;
 import net.minecraft.world.biome.BiomeColors;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.event.ColorHandlerEvent;
-import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import raziel23x.projectskyblock.ProjectSkyblock;
 import raziel23x.projectskyblock.utils.RegistryHandler;
+
 
 
 @Mod.EventBusSubscriber(modid = ProjectSkyblock.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
 public class ClientEvents {
 
-/*
+
     @SubscribeEvent
     public static void registerBlockColors(ColorHandlerEvent.Block event) {
 
+        BlockColors blockcolors = event.getBlockColors();
 
-        BlockColors blockcolors = Minecraft.getInstance().getBlockColors();
+        blockcolors.register((state, reader, pos, i) -> reader != null && pos != null ? BiomeColors.getWaterColor(reader, pos) : 0x3c44a9, RegistryHandler.WATER_GENERATOR_BLOCK.get());
 
-        blockcolors.register((state, reader, pos, color) -> reader != null && pos != null ? BiomeColors.getWaterColor(reader, pos) : -1, RegistryHandler.WATER_GENERATOR_BLOCK.get());
     }
 
-    @Mod(Main.MOD_ID)
-    @Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
-    public class Main {
-        public static final String MOD_ID = "mod_id";
-        public Main() {
-            final IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
-            eventBus.addListener(this::onCommonSetup);
-            eventBus.addListener(this::onClientSetup);
-        }
+    @SubscribeEvent
+    public static void registerItemColors(ColorHandlerEvent.Item event) {
+
+        ItemColors itemcolors = event.getItemColors();
+        BlockColors blockcolors = event.getBlockColors();
+
+
+        itemcolors.register((stack, i) -> {
+            BlockState state = ((BlockItem) stack.getItem()).getBlock().getDefaultState();
+            return blockcolors.getColor(state, null, null, i);
+        }, RegistryHandler.WATER_GENERATOR_BLOCK_ITEM.get());
     }
-*/
+
+
 }
