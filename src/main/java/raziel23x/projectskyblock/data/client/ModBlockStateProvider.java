c3e7391e7929ab2f5b1228c4d507a0962df99b3d
Gerald Baggett <raziel23x@gmail.com>
2020-11-25 20:06:31 -0500

    updated to forge 1.16.4 as well as changed to the datagen and version release 1.0.2

--- src/main/java/raziel23x/projectskyblock/data/client/ModBlockStateProvider.java
+++ src/main/java/raziel23x/projectskyblock/data/client/ModBlockStateProvider.java
@@ -1,7 +1,9 @@
 package raziel23x.projectskyblock.data.client;
 
+import net.minecraft.block.Block;
 import net.minecraft.data.DataGenerator;
 import net.minecraftforge.client.model.generators.BlockStateProvider;
+import net.minecraftforge.client.model.generators.ModelFile;
 import net.minecraftforge.common.data.ExistingFileHelper;
 import raziel23x.projectskyblock.ProjectSkyblock;
 import raziel23x.projectskyblock.init.ModBlocks;
@@ -13,9 +15,23 @@ public class ModBlockStateProvider extends BlockStateProvider {
 
     @Override
     protected void registerStatesAndModels() {
-        simpleBlock(ModBlocks.BLUE_REAGENT_BLOCK.get());
-        simpleBlock(ModBlocks.GREEN_REAGENT_BLOCK.get());
-        simpleBlock(ModBlocks.RED_REAGENT_BLOCK.get());
 
+        registerSimpleBlocks();
+
+
+    }
+
+    private void registerSimpleBlocks() {
+        basicBlock(ModBlocks.BLUE_REAGENT_BLOCK.get());
+        basicBlock(ModBlocks.GREEN_REAGENT_BLOCK.get());
+        basicBlock(ModBlocks.RED_REAGENT_BLOCK.get());
+    }
+
+    private void basicBlock(Block block) {
+        simpleItemBlock(block, cubeAll(block));
+    }
+    private void simpleItemBlock(Block block, ModelFile modelFile) {
+        simpleBlock(block, modelFile);
+        simpleBlockItem(block, modelFile);
     }
 }
